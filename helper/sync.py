107cdb3758400ae16b7fc92d9a8f2904b3f42901
Maximilian Grohmann <e01246408@student.tuwien.ac.at>
2024-11-29 13:29:13 +0100

    remove sync temp files

--- helper/sync.py
+++ helper/sync.py
@@ -11,9 +11,7 @@ from helper.path import (
     LOCAL_DATASET_PATH,
     LOCAL_HELPER_PATH,
     LOCAL_MODEL_PATH,
-    LOCAL_TEMP_PATH,
-    MODEL_PATH,
-    TEMP_PATH,
+    RESULT_PATH,
     get_file_paths_in_folder,
 )
 
@@ -52,7 +50,6 @@ def to_train():
         LOCAL_CONFIG_PATH,
         *get_file_paths_in_folder(LOCAL_MODEL_PATH),
         *get_file_paths_in_folder(LOCAL_DATALOADER_PATH),
-        *get_file_paths_in_folder(LOCAL_TEMP_PATH),
     ]
 
     if INCLUDE_DATASET:
@@ -87,14 +84,13 @@ def from_train():
         f"ssh -p {PORT} -i {IDENTITY_FILE}",
     ]
 
-    dependencies = [MODEL_PATH / "results", TEMP_PATH / "results"]
+    dependencies = [RESULT_PATH]
     dependencies = [
         # cast is important otherwise pathlib gets rid of '.'
         f"{USER}@{HOST}:{REMOTE_BASE_DIR}/./{dependency}"
         for dependency in dependencies
-        if dependency.is_dir()
-        or (dependency.is_file() and dependency not in EXCLUDED_FILES)
     ]
+
     rsync_command.extend(dependencies)
 
     rsync_command.append(LOCAL_BASE_DIR)
