correct fMRI window selection; add single fMRI normalization; allow ----->>Start transfer to training server<<----- sending incremental file list helper/__pycache__/ helper/__pycache__/aws.cpython-312.pyc
          3,456 100%    0.00kB/s    0:00:00
          3,456 100%    0.00kB/s    0:00:00 (xfr#1, to-chk=30/71)
helper/__pycache__/env.cpython-312.pyc

            902 100%  880.86kB/s    0:00:00
            902 100%  880.86kB/s    0:00:00 (xfr#2, to-chk=29/71)
helper/__pycache__/folder.cpython-312.pyc

            776 100%  757.81kB/s    0:00:00
            776 100%  757.81kB/s    0:00:00 (xfr#3, to-chk=28/71)
helper/__pycache__/json.cpython-312.pyc

            809 100%  790.04kB/s    0:00:00
            809 100%  790.04kB/s    0:00:00 (xfr#4, to-chk=27/71)
helper/__pycache__/log.cpython-312.pyc

          3,070 100%    2.93MB/s    0:00:00
          3,070 100%    2.93MB/s    0:00:00 (xfr#5, to-chk=26/71)
helper/__pycache__/system.cpython-312.pyc

          2,150 100%    1.03MB/s    0:00:00
          2,150 100%    1.03MB/s    0:00:00 (xfr#6, to-chk=21/71)
model/
model/Wang_X_2020_Decoding_and_mapping_task_states_of_the_human_brain_via_deep_learning/

sent 13,474 bytes  received 288 bytes  3,058.22 bytes/sec
total size is 82,781  speedup is 6.02
----->>Finished transfer to training server<<-----

A newer release of "Amazon Linux" is available.
  Version 2023.6.20241121:
Run "/usr/bin/dnf check-release-update" for full release and version update info
=============================================================================
AMI Name: Deep Learning Base OSS Nvidia Driver GPU AMI (Amazon Linux 2023)
Supported EC2 instances: G6, Gr6, G6e, P4d, P4de, P5, P5e, P5en (G4Dn, G5 not supported.)
NVIDIA driver version: 560.35.03
CUDA versions available: cuda-12.1 cuda-12.2 cuda-12.3 cuda-12.4 cuda-12.5
Default CUDA version is 12.4

Release notes: https://docs.aws.amazon.com/dlami/latest/devguide/appendix-ami-release-notes.html
AWS Deep Learning AMI Homepage: https://aws.amazo

data/hcp_openacces/dataset.py
@@ -14,11 +14,10 @@ from torch.utils.data import Dataset
 import helper.aws as aws
 import helper.json as json
 from helper.folder import get_files, get_folders
-from helper.system import get_max_workers
+from helper.system import get_num_physical_cores
+
 
 # dataset size for TASK_LIMIT = 1: 660 GB
-
-
 class TaskDataset(Dataset):
     SECRETE_NAME = "ConnectomeDB"
     REGION_NAME = "us-east-1"
@@ -56,14 +55,14 @@ class TaskDataset(Dataset):
     ROOT_DIR = Path(__file__).parent / "data"
     META_PATH = ROOT_DIR / "meta.json"
 
-    def __init__(self, subject_ids, transform=None):
+    def __init__(self, subject_ids, is_train=False):
         if not self.fully_downloaded():
             raise RuntimeError(
                 "Make sure to download the dataset before creating an instance."
             )
 
         self.subject_ids = subject_ids
-        self.transform = transform
+        self.is_train = is_train
 
         self.samples = []
         for subject_id in self.subject_ids:
@@ -78,12 +77,15 @@ class TaskDataset(Dataset):
     def __getitem__(self, idx):
         file_path, task = self.samples[idx]
         data = torch.load(f=file_path, weights_only=True)
-        start_idx = np.random.randint(low=0, high=data.shape[0] - self.WINDOW_WIDTH + 1)
+        if self.is_train:
+            start_idx = np.random.randint(low=0, high=data.shape[0] - self.WINDOW_WIDTH + 1)
+        else:
+            start_idx = 0
         data = data[start_idx : start_idx + self.WINDOW_WIDTH, ...]
+        data /= torch.amax(data)
+        data[~torch.isfinite(data)] = 0
         label = self.TASKS.index(task)
 
-        if self.transform:
-            data = self.transform(data)
 
         return data, label
 
@@ -103,9 +105,6 @@ class TaskDataset(Dataset):
 
     @staticmethod
     def download_dataset(num_workers=4):
-        if num_workers < 1:
-            raise RuntimeError("num_workers must at least be 1.")
-
         os.makedirs(TaskDataset.ROOT_DIR, exist_ok=True)
 
         secrete = aws.get_secret(
@@ -226,7 +225,7 @@ class TaskDataset(Dataset):
                     except FileNotFoundError:
                         continue
                     tfmrt_file = nib.load(filename=temp_tfmrt_file_path)
-                    tfmrt = tfmrt_file.get_fdata()
+                    tfmrt = tfmrt_file.get_fdata(dtype=np.float32)
                     tfmrt_cropped = tfmrt[
                         TaskDataset.BRAIN_BOUNDARIES[
                             "x_start"
@@ -304,4 +303,4 @@ class TaskDataset(Dataset):
 
 
 if __name__ == "__main__":
-    TaskDataset.download_dataset(num_workers=get_max_workers())
+    TaskDataset.download_dataset(num_workers=max(1, get_num_physical_cores() - 1))